c0db58e35f19e59ed4ddf6d6fdc5546bd50bcd28
Adrien Wald <adrien@coloop.ai>
2025-10-17 16:09:33 +0100

    chore: consistency

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './generated/index'
 export * as sdk from './generated/client'
-export { RecallSdk } from './RecallSdk'
-export type { RecallSdkOptions } from './RecallSdk'
+export { RecallSdk } from './sdk'
+export type { RecallSdkOptions } from './sdk'
 export * from './webhooks'
